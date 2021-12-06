bbeb0a90f5a3d79c3f48b7331de74e715d7da360
Philip Meier <github.pmeier@posteo.de>
2021-12-06 10:51:04 +0100

    use deque for easy rotating in 2021-06

--- aoc/year2021/day06.py
+++ aoc/year2021/day06.py
@@ -1,34 +1,27 @@
-from collections import Counter
-
-
-def solve(internal_states, *, days):
-    fishs = Counter(internal_states)
-    for _ in range(days):
-        fishs = dict(
-            zip([internal_state - 1 for internal_state in fishs.keys()], fishs.values())
-        )
-        num_new_fish = fishs.pop(-1, 0)
-        try:
-            fishs[6] += num_new_fish
-        except KeyError:
-            fishs[6] = num_new_fish
-        fishs[8] = num_new_fish
-
-    return sum(fishs.values())
+from collections import Counter, deque
 
 
 def parse_lines(fn):
     def wrapper(lines):
-        return fn([int(internal_state) for internal_state in lines[0].split(",")])
+        return fn([int(initial_state) for initial_state in lines[0].split(",")])
 
     return wrapper
 
 
-@parse_lines
-def part1(internal_states):
-    return solve(internal_states, days=80)
+def solve(initial_states, *, days):
+    counter = Counter(initial_states)
+    fishs = deque([counter.get(state, 0) for state in range(9)])
+    for _ in range(days):
+        fishs.rotate(-1)
+        fishs[6] += fishs[8]
+    return sum(fishs)
 
 
 @parse_lines
-def part2(internal_states):
-    return solve(internal_states, days=256)
+def part1(initial_states):
+    return solve(initial_states, days=80)
+
+
+@parse_lines
+def part2(initial_states):
+    return solve(initial_states, days=256)
